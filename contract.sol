27e1af467b9e9b16dce8af6156ab4abba7781835
Darren <kjchen@protonmail.ch>
2022-01-28 23:47:42 +0800

    UPDATE at 2022/01/28

--- contract.sol
+++ contract.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
+pragma solidity ^0.8.7;
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
 
-contract Marvel1155 is ERC1155PresetMinterPauser {
+contract TEST_NFT_STOCK is ERC1155 {
     
     uint256 public constant XXX_TEST_NFT_STOCK = 0;
     
-    constructor() ERC1155PresetMinterPauser("https://darren-k-chen.github.io/XXX_TEST_NFT_STOCK/{id}.json") {
+    constructor() ERC1155("https://raw.githubusercontent.com/darren-k-chen/XXX_TEST_NFT_STOCK/main/{id}.json") {
         _mint(msg.sender, XXX_TEST_NFT_STOCK, 10**5, "");
     }
-    
 }
